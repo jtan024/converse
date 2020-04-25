96832c73180e7ed709d6a12c76baf4eec91089df
ShayaanAkbar <shayaanakbar1106@gmail.com>
2020-04-25 01:45:19 -0500

    Currently testing Firebase testing is working

--- ConverseBackend/server.js
+++ ConverseBackend/server.js
@@ -3,6 +3,30 @@ const VoiceResponse = require('twilio').twiml.VoiceResponse;
 
 const app = express();
 
+// Firebase
+
+var admin = require("firebase-admin");
+
+var serviceAccount = require("./converse-hacknow-a6513-firebase-adminsdk-im6cm-9b7d8528e9");
+
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: "https://converse-hacknow-a6513.firebaseio.com"
+});
+
+var ref = admin.database().ref('Node-client');
+var messagesRef = ref.child('Test');
+
+var messagesRef = messagesRef.push();
+var messagesRef = messagesRef.push();
+messagesRef.push({
+    name: 'Shayaan',
+    admin: true,
+    count: 1,
+    text: 'Hey guys'
+});
+
+
 // Create a route that will handle Twilio webhook requests, sent as an
 // HTTP POST to /voice in our application
 app.post('/voice', (request, response) => {
