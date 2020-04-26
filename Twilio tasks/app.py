c81234c2a9a71e5742f978caad8c04422a1a9d92
ShayaanAkbar <shayaanakbar1106@gmail.com>
2020-04-25 23:29:04 -0500

    Added Statistics Page

--- Twilio tasks/app.py
+++ Twilio tasks/app.py
@@ -1,23 +1,28 @@
 from flask import Flask
 from test import push_data
+
 app = Flask(__name__)
+
 
 @app.route('/')
 def home():
     return 'Hello World!'
 
+
 from flask import send_file
+
 
 @app.route('/dynamicsay_requests', methods=['POST'])
 def dynamic_say_requests():
     return send_file('dynamicsay_requests.json')
 
+
 import json
 from flask import jsonify, request
 
-@app.route('/collect_requests',  methods=['POST'])
+
+@app.route('/collect_requests', methods=['POST'])
 def collect_requests():
-    
     memory = json.loads(request.form.get('Memory'))
 
     answers = memory['twilio']['collected_data']['collect_clothes_order']['answers']
